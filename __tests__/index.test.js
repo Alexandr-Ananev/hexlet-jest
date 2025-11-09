6aab81ee96044e747ea62dbe15f9a07da87b71c7
Alexandr Ananev <sanyches1978@gmail.com>
2025-11-09 14:57:14 +0100

    Add fixtures text.txt and reversed.txt, update reverse() test to use them

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,19 @@
 import reverse from '../src/index.js'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh')
   expect(reverse('')).toEqual('')
+
+  const text = readFile('text.txt').trim()
+  const result = readFile('reversed.txt').trim()
+
+  expect(reverse(text)).toEqual(result)
 })
